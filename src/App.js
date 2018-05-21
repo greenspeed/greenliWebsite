b74337081b85a1218a49c4e2ffc43e44ae8f4c55
greengege <gelin@clicktocloud.com>
2018-05-21 11:42:09 +1000

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import Layout from './app/Layout'
+
+class App extends Component {
+  render() {
+    return (
+      <Layout />
+    );
+  }
+}
+
+export default App;
